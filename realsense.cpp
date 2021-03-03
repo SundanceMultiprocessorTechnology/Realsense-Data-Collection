ef646bda4dd9d2c544adb2389634fb9539f4d8ca
Jack Bonnell <jack.b@sundance.com>
2021-03-03 17:34:19 +0000

    Inital lo-res changes

--- realsense.cpp
+++ realsense.cpp
@@ -29,8 +29,9 @@ int main(int argc, char * argv[]) try
     // Declare RealSense pipeline, encapsulating the actual device and sensors
     rs2::pipeline pipe;
     rs2::config cfg;
-    cfg.enable_stream(RS2_STREAM_DEPTH,640,480,RS2_FORMAT_Z16);
-    cfg.enable_stream(RS2_STREAM_COLOR,640,480,RS2_FORMAT_BGR8);
+    cfg.enable_stream(RS2_STREAM_INFRARED,  1, 424, 240,RS2_FORMAT_Y8);
+    cfg.enable_stream(RS2_STREAM_DEPTH, 424, 240,RS2_FORMAT_Z16);
+    cfg.enable_stream(RS2_STREAM_COLOR, 320, 240,RS2_FORMAT_Y16);
     cfg.enable_record_to_file("./bags/" + std::to_string(t) + ".bag");
     // Start streaming with default recommended configuration
     rs2::pipeline_profile profile = pipe.start(cfg);
@@ -41,6 +42,7 @@ int main(int argc, char * argv[]) try
     //If there is no color stream, choose to align depth to another stream
     rs2::device dev = profile.get_device();
     rs2::depth_sensor ds = dev.query_sensors().front().as<rs2::depth_sensor>();
+    ds.set_option(RS2_OPTION_EMITTER_ENABLED, 0.f); // Disable emitter
     clock_t clockstarted = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
     int timeintervals = 1;
     
@@ -65,7 +67,7 @@ int main(int argc, char * argv[]) try
         std::cout<<"Bag running for:"<<clocknow<< " seconds" <<std::endl;
         }
 
-        if (clocknow >= (30)){
+        if (clocknow >= (10)){
             pipe.stop();
             std::cout << "Pipeline Stopped..." << std::endl;
             cfg.enable_record_to_file("./bags/" + std::to_string(t) + ":" + std::to_string(timeintervals) +".bag");
