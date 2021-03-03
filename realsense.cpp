97a9d513023ec2601d862a9914e1379fe612625b
Jack Bonnell <jack.b@sundance.com>
2021-03-03 10:45:45 +0000

    bug fix: remove ostream

--- realsense.cpp
+++ realsense.cpp
@@ -3,6 +3,7 @@
 // Email: jack.b@sundance.com
 //Website: www.sundance.com
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+#include <iostream>
 #include <glog/logging.h>
 #include <memory>
 #include <chrono>
