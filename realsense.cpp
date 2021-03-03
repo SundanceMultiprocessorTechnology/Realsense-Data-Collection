01c5abb05c65ecde40cd37639ebc2b85c407f9d5
Jack Bonnell <jack.b@sundance.com>
2021-03-03 12:16:24 +0000

    add bag split

--- realsense.cpp
+++ realsense.cpp
@@ -4,13 +4,13 @@
 //Website: www.sundance.com
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <iostream>
-#include <glog/logging.h>
 #include <memory>
 #include <chrono>
-
+#include <thread>
 using namespace std;
 int main(int argc, char * argv[]) try
 {
+    
     std::cout << "Realsense Vitis AI Demo" << std::endl;
     std::cout << "By Jack Bonnell" << std::endl; 
     std::cout << "Email: Jack.B@sundance.com" << std::endl;
@@ -23,7 +23,7 @@ int main(int argc, char * argv[]) try
     rs2::config cfg;
     cfg.enable_stream(RS2_STREAM_DEPTH,640,480,RS2_FORMAT_Z16);
     cfg.enable_stream(RS2_STREAM_COLOR,1280,720,RS2_FORMAT_BGR8);
-    cfg.enable_record_to_file("./test.bag");
+    cfg.enable_record_to_file("./" + std::to_string(t) + ".bag");
     // Start streaming with default recommended configuration
     rs2::pipeline_profile profile = pipe.start(cfg);
     // Each depth camera might have different units for depth pixels, so we get it here
@@ -33,21 +33,40 @@ int main(int argc, char * argv[]) try
     //If there is no color stream, choose to align depth to another stream
     rs2::device dev = profile.get_device();
     rs2::depth_sensor ds = dev.query_sensors().front().as<rs2::depth_sensor>();
+    clock_t clockstarted = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    int timeintervals = 1;
     while (true)
     {
-        clock_t beginFrame = clock();
+        
+        clock_t clocknow =std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count() - clockstarted;
+        
+        clock_t beginFrame = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
 	    data = align_to_color.process(data);
 	    rs2::video_frame rgb = data.get_color_frame();
         uint8_t* rgb_data = (uint8_t*)rgb.get_data();
         rs2::depth_frame depth = data.get_depth_frame();
         uint16_t* depth_data = (uint16_t*)depth.get_data();
-        clock_t endFrame = clock();
-        double fps = 1/(endFrame-beginFrame);
+        clock_t endFrame = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        double fps = 1000/(endFrame-beginFrame);
+        if (fps < 60){
+        std::system("clear");
         std::cout<<"FrameTime was:"<<fps<<std::endl;
+        std::cout<<"Bag running for:"<<clocknow<< " seconds" <<std::endl;
+        }
+
+        if (clocknow >= (30)){
+            pipe.stop();
+            std::cout << "Pipeline Stopped..." << std::endl;
+            cfg.enable_record_to_file("./" + std::to_string(t) + ":" + std::to_string(timeintervals) +".bag");
+            std::cout << "Pipeline Starting..." << std::endl;
+            std::this_thread::sleep_for(chrono::seconds(5));
+            rs2::pipeline_profile profile = pipe.start(cfg);
+            clockstarted = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+            timeintervals = timeintervals + 1;
+        }
     }
-    pipe.stop();
-    std::cout << "Pipeline Stopped..." << std::endl;
     return EXIT_SUCCESS;
 }
 catch (const rs2::error & e)
